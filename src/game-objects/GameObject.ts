660ceec2036a4f58596940bba796b13f6a04279f
Tomas Esconjaureguy <tomas.esconjaureguy@mediamonks.com>
2020-02-01 10:50:31 -0300

    Add GameObject

---
+++ src/game-objects/GameObject.ts
@@ -0,0 +1,7 @@
+import Phaser from "phaser";
+
+export abstract class GameObject {
+  public abstract load(scene: Phaser.Scene): void;
+  public abstract initialize(scene: Phaser.Scene): void;
+  public update(): void {}
+}
