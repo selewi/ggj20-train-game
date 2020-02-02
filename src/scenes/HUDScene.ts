a64b4cb09496e6769dd67c8fe262e2b0be180d26
Enio Lotero <eniolotero@gmail.com>
2020-02-02 17:30:21 -0300

    Adds aim cursor

--- src/scenes/HUDScene.ts
+++ src/scenes/HUDScene.ts
@@ -8,9 +8,11 @@ export enum HUDSceneEvents {
 
 export class HUDScene extends Phaser.Scene {
   private static vignetteKey = uiAssets.vignette.toString();
+  private static aimKey = uiAssets.aim.toString();
 
   private livesText: Phaser.GameObjects.Text;
   private vignette: Phaser.GameObjects.Image;
+  private aim: Phaser.GameObjects.Image;
 
   constructor() {
     super(sceneConfig);
@@ -18,6 +20,7 @@ export class HUDScene extends Phaser.Scene {
 
   public preload() {
     this.load.image(HUDScene.vignetteKey, uiAssets.vignette);
+    this.load.image(HUDScene.aimKey, uiAssets.aim);
   }
 
   public create() {
@@ -33,11 +36,21 @@ export class HUDScene extends Phaser.Scene {
     this.vignette.setDisplaySize(1280, 720);
     this.vignette.alpha = 0.9;
     this.vignette.setDepth(-1);
+
+    this.aim = this.add.image(this.game.scale.width / 2, 590, uiAssets.aim);
+    //this.vignette.setOrigin(0, 0);
+    //this.vignette.setDisplaySize(1280, 720);
+    this.aim.alpha = 0.75;
+    this.aim.setDepth(-0.9);
   }
 
   public showLives = (amount: number) => {
     this.livesText.setText(`Lives: ${amount}`);
   };
+
+  public update() {
+    this.aim.angle += 4;
+  }
 }
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
