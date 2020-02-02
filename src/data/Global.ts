0c8e7c01dcff578353d7c139d35c92fe78adb8fb
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2020-02-02 16:17:11 -0300

    fix particles

--- src/data/Global.ts
+++ src/data/Global.ts
@@ -2,15 +2,14 @@ export const startingLives = 3;
 export const speedFactor = 0.01;
 export const zIndex = {
   background: 0,
-  backFloor: 49,
+  backFloor: 10,
   railTable: 50,
   topRail: 100,
   topRivet: 105,
   train: 150,
   bottomRail: 160,
-
   bottomRivet: 165,
-  sparkParticles: 170,
   character: 200,
-  froontFloor: 220
+  froontFloor: 220,
+  sparkParticles: 225
 };
--- src/game-objects/Particles.ts
+++ src/game-objects/Particles.ts
@@ -1,5 +1,6 @@
 import { GameObject } from "./GameObject";
 import { spriteAssets } from "../../assets/index";
+import { zIndex } from "../data/Global";
 
 export class Particles extends GameObject {
   public static smokeParticleSpriteKey = spriteAssets.smokeParticle;
@@ -15,15 +16,17 @@ export class Particles extends GameObject {
     scale: { start: 0.2, end: 0.7, ease: "Power4" },
     rotate: this.random(0, 135)
   };
+  public smokeParticle: Phaser.GameObjects.Particles.ParticleEmitterManager;
+  public sparkParticle: Phaser.GameObjects.Particles.ParticleEmitterManager;
   public sparkEmitter: Phaser.GameObjects.Particles.ParticleEmitter;
   public smokeEmitter: Phaser.GameObjects.Particles.ParticleEmitter;
 
   public sparkEmitterConfig: Phaser.Types.GameObjects.Particles.ParticleEmitterConfig = {
     frequency: 1,
     x: { min: 300, max: 350 },
-    y: { min: 600, max: 630 },
+    y: { min: 580, max: 600 },
     lifespan: { min: 250, max: 800 },
-    speedY: { min: -10, max: 60 },
+    speedY: { min: -260, max: -120 },
     speedX: { min: -1500, max: 10 },
     gravityY: 1000,
     gravityX: -500,
@@ -45,11 +48,16 @@ export class Particles extends GameObject {
   public update = () => {};
 
   public initialize = (scene: Phaser.Scene) => {
-    let smokeParticle = scene.add.particles(Particles.smokeParticleSpriteKey);
-    this.smokeEmitter = smokeParticle.createEmitter(this.smokeEmitterConfig);
+    this.smokeParticle = scene.add.particles(Particles.smokeParticleSpriteKey);
+    this.smokeEmitter = this.smokeParticle.createEmitter(
+      this.smokeEmitterConfig
+    );
 
-    let sparkParticles = scene.add.particles(Particles.sparkParticleSpriteKey);
-    this.sparkEmitter = sparkParticles.createEmitter(this.sparkEmitterConfig);
+    this.sparkParticle = scene.add.particles(Particles.sparkParticleSpriteKey);
+    this.sparkEmitter = this.sparkParticle.createEmitter(
+      this.sparkEmitterConfig
+    );
+    this.sparkParticle.setDepth(zIndex.sparkParticles);
   };
 
   public random(min: number, max: number) {
