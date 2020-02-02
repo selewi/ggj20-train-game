05d7bdedb92a685f55e54217b11cb247deb92fed
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2020-02-02 15:57:53 -0300

    add floor

--- src/data/Global.ts
+++ src/data/Global.ts
@@ -2,12 +2,15 @@ export const startingLives = 3;
 export const speedFactor = 0.01;
 export const zIndex = {
   background: 0,
+  backFloor: 49,
   railTable: 50,
   topRail: 100,
   topRivet: 105,
   train: 150,
   bottomRail: 160,
+
   bottomRivet: 165,
   sparkParticles: 170,
-  character: 200
+  character: 200,
+  froontFloor: 220
 };
